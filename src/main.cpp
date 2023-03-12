0dd150255933c7f4e5acfca968f3060ba90a199b
Jie Xiang <n.jiexiang@gmail.com>
2023-03-13 02:07:27 +0800

    Created Window class with basic functionality

--- src/main.cpp
+++ src/main.cpp
@@ -2,4 +2,14 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
-#include "hello.cpp"
+#include "window.cpp"
+
+int main()
+{
+    Window window = Window(800, 600, "Graphics Engine");
+    bool success = window.createWindow();
+    if (success)
+    {
+        window.run();
+    }
+}
---
+++ src/window.cpp
@@ -0,0 +1,103 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <iostream>
+
+class Window
+{
+private:
+    uint32_t width, height;
+    const char *windowName;
+    GLFWwindow *window;
+
+    static void framebufferSizeCallback(GLFWwindow *window, int width, int height);
+    static void processInput(GLFWwindow *window);
+
+public:
+    Window(uint32_t width, uint32_t height, const char *windowName);
+    ~Window();
+
+    bool createWindow();
+    void render();
+    void run();
+};
+
+Window::Window(uint32_t width, uint32_t height, const char *windowName)
+    : width(width), height(height), windowName(windowName)
+{
+}
+
+Window::~Window()
+{
+    glfwTerminate();
+}
+
+bool Window::createWindow()
+{
+    // Initialize glfw
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+    // Create glfw window
+    window = glfwCreateWindow(width, height, windowName, NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        this->~Window();
+        return false; // TODO: use an enum instead?
+    }
+
+    // Register resize callback and set current context
+    glfwSetFramebufferSizeCallback(window, Window::framebufferSizeCallback);
+    glfwMakeContextCurrent(window);
+
+    // Initialize GLAD
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        this->~Window();
+        return false;
+    }
+    return true;
+}
+
+void Window::framebufferSizeCallback(GLFWwindow *window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+}
+
+void Window::processInput(GLFWwindow *window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+    {
+        glfwSetWindowShouldClose(window, GL_TRUE);
+    }
+}
+
+void Window::render()
+{
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+}
+
+void Window::run()
+{
+    while (!glfwWindowShouldClose(window))
+    {
+        // Process key inputs
+        processInput(window);
+
+        // Run render commands
+        render();
+
+        // Check for events and swap buffers;
+        glfwPollEvents();
+        glfwSwapBuffers(window);
+    }
+    this->~Window();
+}
