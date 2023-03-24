88f409a3313a686053ddd4c6d970c1f6e4c7296b
Jie Xiang <n.jiexiang@gmail.com>
2023-03-24 18:04:55 +0800

    Removed redundant code, fixed glTexParameteri error, and added more gnu compiler flags in debug mode

--- src/main.cpp
+++ src/main.cpp
@@ -11,48 +11,7 @@ int main() {
     if (!success) {
         return 1;
     }
-
-    // // clang-format off
-    // std::vector<float> vertices1 = {
-    //     // positions          // colors           // texture coords
-    //      0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
-    //      0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
-    //     -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
-    //     -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left 
-    // };
-
-    // std::vector<float> vertices2 = {
-    //     -1.0f, -0.5f, 0.0f,  // bottom left
-    //      0.0f, -0.5f, 0.0f,  // bottom midddle  
-    //     -0.5f,  0.5f, 0.0f,  // top left
-    //      1.0f, -0.5f, 0.0f,  // bottom right
-    //      0.5f,  0.5f, 0.0f,  // top right
-    // };
-
-    // std::vector<uint32_t> indices1 = {
-    //     0, 1, 2,   // first triangle
-    //     2, 3, 0    // second triangle
-    // };
-
-    // std::vector<uint32_t> indices2 = {
-    //     0, 1, 2,   // first triangle
-    //     1, 3, 4    // second triangle
-    // };
-    // // clang-format on
-
-    // window.setVertices(vertices1);
-    // window.addVertexAttribute(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), 0);
-    // window.addVertexAttribute(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), 3 * sizeof(float));
-    // window.addVertexAttribute(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), 6 * sizeof(float));
-    // window.setIndices(indices1);
-    // window.createShaderProgram("res/vertex.glsl", "res/fragment.glsl");
-    // uint32_t texture1 = window.loadTexture("res/container.jpg", GL_TEXTURE0, GL_RGB);
-    // uint32_t texture2 = window.loadTexture("res/awesomeface.png", GL_TEXTURE1, GL_RGBA);
-
-    // glActiveTexture(GL_TEXTURE0);
-    // glBindTexture(GL_TEXTURE_2D, texture1);
-    // glActiveTexture(GL_TEXTURE1);
-    // glBindTexture(GL_TEXTURE_2D, texture2);
-
     window.run();
+    int i = 23;
+    i <<= 32;
 }
