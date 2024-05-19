957fc8dbf6f973cbf884af4329321e3bedff30d8
Jie Xiang <n.jiexiang@gmail.com>
2024-05-19 18:07:10 +0800

    Add debug message callback

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,17 @@
 #include <GLFW/glfw3.h>
 
 #include <iostream>
+#include <string>
+#include <cstdio>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void process_input(GLFWwindow* window);
+void GLAPIENTRY error_message_callback(GLenum source, GLenum type, GLuint id, GLenum severity,
+        GLsizei length, const GLchar* message, const void* userParam);
 
 int main(void) {
     glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
@@ -27,6 +31,12 @@ int main(void) {
 
     glViewport(0, 0, 800, 600);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+#ifdef DEBUG
+    glEnable(GL_DEBUG_OUTPUT);
+    std::cout << "[OpenGL] Version: " << glGetString(GL_VERSION) << std::endl;
+    glDebugMessageCallback(error_message_callback, nullptr);
+#endif
 
     while (!glfwWindowShouldClose(window)) {
         process_input(window);
@@ -52,4 +62,62 @@ void process_input(GLFWwindow* window) {
     if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
         glfwSetWindowShouldClose(window, true);
     }
+}
+
+void GLAPIENTRY error_message_callback(GLenum source, GLenum type, GLuint id, GLenum severity,
+        GLsizei length, const GLchar* message, const void* userParam) {
+    std::string source_str, type_str, severity_str;
+    (void) id, (void) length, (void) userParam;
+
+    switch (source) {
+        case GL_DEBUG_SOURCE_API:
+            source_str = "OpenGL API call";
+            break;
+        case GL_DEBUG_SOURCE_WINDOW_SYSTEM:
+            source_str = "Window-system API call";
+            break;
+        case GL_DEBUG_SOURCE_SHADER_COMPILER:
+            source_str = "Shader compilation";
+            break;
+        default:
+            source_str = "Other";
+    }
+
+    switch (type) {
+        case GL_DEBUG_TYPE_ERROR:
+            type_str = "Error";
+            break;
+        case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR:
+            type_str = "Deprecated behavior";
+            break;
+        case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:
+            type_str = "Undefined behavior";
+            break;
+        case GL_DEBUG_TYPE_PORTABILITY:
+            type_str = "Portability";
+            break;
+        case GL_DEBUG_TYPE_PERFORMANCE:
+            type_str = "Performance";
+            break;
+        default:
+            type_str = "Other";
+    }
+
+    switch (severity) {
+        case GL_DEBUG_SEVERITY_HIGH:
+            severity_str = "High";
+            break;
+        case GL_DEBUG_SEVERITY_MEDIUM:
+            severity_str = "Medium";
+            break;
+        case GL_DEBUG_SEVERITY_LOW:
+            severity_str = "Low";
+            break;
+        case GL_DEBUG_SEVERITY_NOTIFICATION:
+            severity_str = "Notification";
+            break;
+    }
+
+    std::fprintf(stderr, "[OpenGL] %s: %s. Severity: %s. Message: %s\n",
+            type_str.c_str(), source_str.c_str(), severity_str.c_str(), message);
 }
